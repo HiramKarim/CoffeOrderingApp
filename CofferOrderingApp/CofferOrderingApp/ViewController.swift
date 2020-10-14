56aee9da379e85df575ee05081fc9e7ffbf1dd6d
Hiram Castro <hiram2027@gmail.com>
2020-10-13 22:14:08 -0700

    initial commit
    adding project

---
+++ CofferOrderingApp/CofferOrderingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CofferOrderingApp
+//
+//  Created by Hiram Castro on 10/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
