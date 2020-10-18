2b40640342c965a8a99e175160139908508ba842
Hiram Castro <hiram2027@gmail.com>
2020-10-18 01:13:17 -0700

    changes to storyboard
    + adding main flow
    + adding service manager to call api

--- CofferOrderingApp/CofferOrderingApp/Controllers/AddOrdersVC.swift
+++ CofferOrderingApp/CofferOrderingApp/Controllers/AddOrdersVC.swift
@@ -8,22 +8,28 @@
 import UIKit
 
 class AddOrdersVC: UIViewController {
-
+    
+    @IBOutlet weak var ordersListTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        ordersListTable.delegate = self
+        ordersListTable.dataSource = self
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension AddOrdersVC: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-    */
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        //cell.backgroundColor = .red
+        return cell
+    }
 
 }
---
+++ CofferOrderingApp/CofferOrderingApp/Helper/JSONParser.swift
@@ -0,0 +1,8 @@
+//
+//  JSONParser.swift
+//  CofferOrderingApp
+//
+//  Created by Hiram Castro on 10/18/20.
+//
+
+import Foundation
