51e4cc047ea82f71d4320d495aac134fb4069b45
Hiram Castro <hiram2027@gmail.com>
2020-10-24 23:33:57 -0700

    adding functionality to show / hide keyboard
    + Adding functionality to show / hide keyboard and move the view on y position by using the keyboard height
    
    + adding helper for keyboard notifications

--- CofferOrderingApp/CofferOrderingApp/Controllers/AddOrdersVC.swift
+++ CofferOrderingApp/CofferOrderingApp/Controllers/AddOrdersVC.swift
@@ -16,6 +16,8 @@ class AddOrdersVC: UIViewController {
     
     private var addCoffeeOrderVM = AddCoffeeOrderVM()
     
+    private var keyboardHelper: KeyboardHelper?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,6 +29,13 @@ class AddOrdersVC: UIViewController {
         self.ordersListTable.estimatedRowHeight = UITableView.automaticDimension
         
         populateSegmentedControl()
+        setupPrivateKeyboardHelper()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTouchOnView))
+        tapGesture.numberOfTapsRequired = 1
+        self.view.addGestureRecognizer(tapGesture)
+        
+        self.view.isUserInteractionEnabled = true
     }
     
     private func populateSegmentedControl() {
@@ -35,13 +44,49 @@ class AddOrdersVC: UIViewController {
         }
     }
     
+    private func setupPrivateKeyboardHelper() {
+        keyboardHelper = KeyboardHelper {
+            [unowned self] animation, keyboardFrame, duration in
+            switch animation {
+            case .keyboardWillShow:
+                self.keyboardWillAppear(keyboardFrame: keyboardFrame)
+                break
+            case .keyboardWillHide:
+                break
+            }
+        }
+    }
+    
+    private func keyboardWillAppear(keyboardFrame:CGRect) {
+        if view.frame.origin.y == 0 {
+            self.view.frame.origin.y -= keyboardFrame.height
+        }
+    }
+
+    private func keyboardWillDisappear() {
+        if view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+            self.NameTextfield.resignFirstResponder()
+            self.EmailTextfield.resignFirstResponder()
+        }
+    }
+    
+    @objc private func handleTouchOnView() {
+        keyboardWillDisappear()
+    }
+    
     @IBAction func SaveButtonPressed(_ sender: Any) {
+        
+        let customerName = NameTextfield.text
+        let customerEmail = EmailTextfield.text
+        let selectedSize = addCoffeeOrderVM.sizes[CoffeeSizeSegmentedControl.selectedSegmentIndex]
+        let selectedCoffeeType = addCoffeeOrderVM.tyes[0]
         
     }
     
 }
 
-extension AddOrdersVC: UITableViewDelegate, UITableViewDataSource {
+extension AddOrdersVC: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return addCoffeeOrderVM.tyes.count
@@ -53,4 +98,16 @@ extension AddOrdersVC: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
 
+}
+
+extension AddOrdersVC: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
+    }
+    
 }
